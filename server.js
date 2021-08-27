4d2d17a007abf31dcdc1329223c77689d54ba819
LiveTheFuture <ademijutobi@gmail.com>
2021-08-27 10:57:32 +0100

    First commit

--- server.js
+++ server.js
@@ -5,11 +5,19 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+const requestIp = require('request-ip');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus:200}))  // some legacy browsers choke on 204
+
+var ipMiddleware = function(req, res, next){
+  const clientIp = requestIp.clientIp(req);
+  next();
+}
+
+app.use(requestIp.mw());
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,15 +27,20 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  const ipAddress = req.clientIp;
+  const language = req.acceptsLanguages();
+  const software = req.get('User-Agent');
+
+  res.json({
+    ipAddress: ipAddress,
+    language: language[0],
+    software: software
+  });
 });
 
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env)
 });
